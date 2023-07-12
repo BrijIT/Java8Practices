c8ab08b18937be57aba0ea5d421cb793611728d4
Brij mohan <mohan.bri47@gmail.com>
2023-07-12 20:04:22 +0530

    Added Cooment Ambiguity

--- src/JPMorganInterview/Question3.java
+++ src/JPMorganInterview/Question3.java
@@ -2,24 +2,23 @@ package JPMorganInterview;
 
 public class Question3 {
 
-   
-
     public static void main(String[] args) {
         class A {
             public void fun(Integer i) {
                 System.out.println("Integer");
-    
+
             }
-    
+
             public void fun(String i) {
                 System.out.println("String");
-    
+
             }
         }
 
         A a = new A();
         a.fun(null);
-
+        // Ambiguity Error Because Integer and String both are wrapper classes and both
+        // has default value null
 
     }
 
